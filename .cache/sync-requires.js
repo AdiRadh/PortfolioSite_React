323661f2faee6802ca05ccd25d8c4e870625f613
Adi <adiradh1998@gmail.com>
2020-09-30 19:08:47 +0100

    clean and build

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("D:\\Projects\\PortfolioSite\\Git_version\\PortfolioSite_React\\.cache\\dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("D:\\Projects\\PortfolioSite\\Git_version\\PortfolioSite_React\\src\\pages\\404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("D:\\Projects\\PortfolioSite\\Git_version\\PortfolioSite_React\\src\\pages\\index.js"))),
   "component---src-pages-personal-index-js": hot(preferDefault(require("D:\\Projects\\PortfolioSite\\Git_version\\PortfolioSite_React\\src\\pages\\personal\\index.js")))
